170782db0e85d18ae71570d204d85a273f3150d5
Martin Suasnavar <msuasnavar21@gmail.com>
2024-03-11 00:43:40 -0300

    Responsive design

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -19,9 +19,9 @@ interface RootLayoutProps {
 
 /* reusable classes */
 // contact box
-const contactBox = "rounded bg-gradient-to-r from-pink-900 to-indigo-900  w-40 h-30 m-4 p-5  text-center transition-transform duration-300 transform hover:scale-105";
+const contactBox = "rounded bg-gradient-to-r from-pink-900 to-indigo-900  w-20 sm:w-40 h-30 sm:h-30 m-4 p-1 sm:p-5 text-center transition-transform duration-300 transform hover:scale-105 ";
 const cBoxContent = "flex flex-col justify-center items-center ml-auto mr-auto";
-const cBContentText = "mt-auto";
+const cBContentText = "hidden sm:block mt-auto";
 
 export default function RootLayout({
   children,
@@ -41,48 +41,53 @@ export default function RootLayout({
       <nav><NavBar/></nav>
       
       {children}
-      <footer className="p-10 justify-center flex">
-        <section className="w-80">
-          <div className="text-3xl font-bold">
-          {t('footer.contactMe')}
-          </div>
-          <div>
-          {t('footer.anyInquiere')}
-          </div>
-        </section>
-        
-        {/*warning: responsive design issues, 
-        the following part regarding contact 
-        boxes creates an empty blank space on
-         the right side of the page*/}
-        <section className="flex">
+      <footer className="p-10 justify-center ">
+        <div className="text-3xl mr-auto ml-auto flex justify-center sm:hidden">
+            {t('footer.contactMe')}
+        </div>
+        <div className="flex ">
+          <section className="w-80 hidden sm:block">
+            <div className="text-sm font-bold sm:text-3xl">
+            {t('footer.contactMe')}
+            </div>
+            <div>
+            {t('footer.anyInquiere')}
+            </div>
+          </section>
+          
+          {/*warning: responsive design issues, 
+          the following part regarding contact 
+          boxes creates an empty blank space on
+          the right side of the page*/}
+          <section className="flex mr-auto ml-auto">
 
-            <a href="https://github.com/martinsuasnavar" className={contactBox}>
-              <sub className={cBoxContent}>
-                <Image src="/logos/github-white.png" height={50} width={50} alt="github logo" />
-                <div className="h-5"/>
-                <text className={cBContentText}>martinsuasnavar</text>
-              </sub>
-            </a>
+              <a href="https://github.com/martinsuasnavar" className={contactBox}>
+                <sub className={cBoxContent}>
+                  <Image src="/logos/github-white.png" height={50} width={50} alt="github logo" />
+                  <div className="h-5"/>
+                  <text className={cBContentText}>martinsuasnavar</text>
+                </sub>
+              </a>
 
-            <a href="https://www.linkedin.com/in/martin-suasnavar-579a4726a/" className={contactBox}>
-              <sub className={cBoxContent}>
-                <Image src="/logos/linkedin-white.png" height={50} width={50} alt="linkedin logo" />
-                <div className="h-5"/>
-                <text className="absolute bottom-0">Martín Suasnavar</text>
-              </sub>
-            </a>
+              <a href="https://www.linkedin.com/in/martin-suasnavar-579a4726a/" className={contactBox}>
+                <sub className={cBoxContent}>
+                  <Image src="/logos/linkedin-white.png" height={50} width={50} alt="linkedin logo" />
+                  <div className="h-5"/>
+                  <text className={cBContentText}>Martín Suasnavar</text>
+                </sub>
+              </a>
 
-            {/*must include a send mail function contained in a page, which shall be redirected from this contact box*/}
-            <Link href={`/${locale}/mail`} className={contactBox}>
-              <sub className={cBoxContent}>
-                <Image src="/logos/gmail-white.png" height={50} width={50} alt="gmail logo" />
-                <div className="h-5"/>
-                <div>msuasnavar21@gmail.com</div>
-              </sub>
-            </Link>
+              {/*must include a send mail function contained in a page, which shall be redirected from this contact box*/}
+              <Link href={`/${locale}/mail`} className={contactBox}>
+                <sub className={cBoxContent}>
+                  <Image src="/logos/gmail-white.png" height={50} width={50} alt="gmail logo" />
+                  <div className="h-5"/>
+                  <div className={cBContentText}>msuasnavar21@gmail.com</div>
+                </sub>
+              </Link>
 
-         </section>
+          </section>
+         </div>
        </footer>
       </body>
      
