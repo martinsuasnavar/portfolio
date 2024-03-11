a14946482538279f800d203402dcc1c4117c68f1
Martin Suasnavar <msuasnavar21@gmail.com>
2024-03-11 13:04:55 -0300

    Update white-box.tsx

--- src/app/components/white-box.tsx
+++ src/app/components/white-box.tsx
@@ -8,7 +8,7 @@ export default function WhiteBox({ children }: WhiteBoxProps) {
     
 
     return (
-    <div className={`bg-white text-black rounded-lg inline-block p-1.5`}>
+    <div className={`bg-white text-black inline-block rounded-lg p-1.5`}>
       {children}
     </div>
   );
