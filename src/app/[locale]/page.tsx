b93ea4fa4cfeabd34a0fbc7d24b180df861f4715
martinsuasnavar <msuasnavar21@gmail.com>
2024-04-22 16:48:51 -0300

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,6 +12,18 @@ interface Tech {
   link: string;
 }
 
+interface FrontTech {
+  array: Tech;
+}
+
+interface BackTech {
+  array: Tech;
+}
+
+interface DBTech {
+  array: Tech;
+}
+
 interface Project {
     imgSrc: string;
     name: string;
@@ -21,6 +33,7 @@ interface Project {
     git: string;
     app: string;
     screen: string;
+    tech: number[];
 }
 
 export default function Home() {
@@ -31,6 +44,67 @@ export default function Home() {
   const t = useTranslations("Home");
 
   const techPath = "/logos/tech";
+  const frontTechArray: Tech[] = [
+    {
+      imgSrc: `${techPath}/react.png`,
+      name: "React.js",
+      link: "https://react.dev/",
+    },
+    {
+      imgSrc: `${techPath}/next.png`,
+      name: "Next.js",
+      link: "https://nextjs.org/",
+    },
+  ]
+  const backTechArray: Tech[] = [
+    {
+      imgSrc: `${techPath}/node.png`,
+      name: "Node.js",
+      link: "https://nodejs.org/",
+    },
+    {
+      imgSrc: `${techPath}/express.png`,
+      name: "Express.js",
+      link: "https://expressjs.com/",
+    },
+    {
+      imgSrc: `${techPath}/php.png`,
+      name: "PHP",
+      link: "https://www.php.net/",
+    },
+    {
+      imgSrc: `${techPath}/laravel.png`,
+      name: "Laravel",
+      link: "https://laravel.com/",
+    },
+    {
+      imgSrc: `${techPath}/python.png`,
+      name: "Python",
+      link: "https://www.python.org/",
+    },
+    {
+      imgSrc: `${techPath}/django.png`,
+      name: "Django",
+      link: "https://www.djangoproject.com/",
+    },
+  ]
+  const dbTechArray: Tech[] = [
+    {
+      imgSrc: `${techPath}/mysql.png`,
+      name: "MySQL",
+      link: "https://www.djangoproject.com/",
+    },
+    {
+      imgSrc: `${techPath}/mongodb.png`,
+      name: "MongoDB",
+      link: "https://www.djangoproject.com/",
+    },
+    {
+      imgSrc: `${techPath}/postgre.png`,
+      name: "PostgreSQL",
+      link: "https://www.djangoproject.com/",
+    }
+  ]
   const techArray: Tech[] = [
     {
       imgSrc: `${techPath}/react.png`,
@@ -71,6 +145,21 @@ export default function Home() {
       imgSrc: `${techPath}/django.png`,
       name: "Django",
       link: "https://www.djangoproject.com/",
+    },
+    {
+      imgSrc: `${techPath}/mysql.png`,
+      name: "MySQL",
+      link: "https://www.mysql.com/",
+    },
+    {
+      imgSrc: `${techPath}/mongodb.png`,
+      name: "MongoDB",
+      link: "https://www.mongodb.com/",
+    },
+    {
+      imgSrc: `${techPath}/postgre.png`,
+      name: "PostgreSQL",
+      link: "https://www.postgresql.org/",
     }
   ]
 
@@ -83,7 +172,8 @@ export default function Home() {
       info: ``,
       git: "https://github.com/martinsuasnavar/panelboard-lite",
       app: "https://panelboard-lite.vercel.app",
-      screen: "/screenshots/0.png"
+      screen: "/screenshots/0.png",
+      tech: [0,2,3,9],
     },
     {
       imgSrc: "/project-2.png",
@@ -93,7 +183,8 @@ export default function Home() {
       info: ``,
       git: "https://github.com/martinsuasnavar/leaf-messenger",
       app: "https://leaf-messenger.vercel.app",
-      screen: "/screenshots/1.png"
+      screen: "/screenshots/1.png",
+      tech: [0,4,5,8],
     }
   ];
 
@@ -173,7 +264,25 @@ export default function Home() {
             </FadingSection>
             <FadingSection subfix={'1'}>
             <div className="flex justify-center items-center">
-            {techArray.map(tech => (
+            {frontTechArray.map(tech => (
+              <a key={tech.name} href={tech.link} className="cursor-pointer m-4 items-center align-center flex flex-col justify-center text-white text-opacity-0 hover:text-opacity-100 transition-transform duration-300 transform hover:scale-105">
+                <Image src={tech.imgSrc} alt={`${tech.name} logo`} width={40} height={40}/>
+                <div className="text-sm mt-4 align-center">{tech.name}</div>
+              
+              </a>
+            ))}
+            </div>
+            <div className="flex justify-center items-center">
+            {backTechArray.map(tech => (
+              <a key={tech.name} href={tech.link} className="cursor-pointer m-4 items-center align-center flex flex-col justify-center text-white text-opacity-0 hover:text-opacity-100 transition-transform duration-300 transform hover:scale-105">
+                <Image src={tech.imgSrc} alt={`${tech.name} logo`} width={40} height={40}/>
+                <div className="text-sm mt-4 align-center">{tech.name}</div>
+              
+              </a>
+            ))}
+            </div>
+            <div className="flex justify-center items-center">
+            {dbTechArray.map(tech => (
               <a key={tech.name} href={tech.link} className="cursor-pointer m-4 items-center align-center flex flex-col justify-center text-white text-opacity-0 hover:text-opacity-100 transition-transform duration-300 transform hover:scale-105">
                 <Image src={tech.imgSrc} alt={`${tech.name} logo`} width={40} height={40}/>
                 <div className="text-sm mt-4 align-center">{tech.name}</div>
@@ -218,11 +327,18 @@ export default function Home() {
                            <Image className="rounded-xl" src={project.screen} height={2000} width={2000} alt={`${project.name} screenshot`}/>
                         }
                         </div>
-                        <div className="h-10"/>
-                       
+                        <div className="h-5"/>
+                        <div className="flex justify-center items-center">
+                          {project.tech.map(tech => (
+                           <div className="m-2">
+                              <Image src={techArray[tech].imgSrc} alt={`${techArray[tech].name} logo`} width={40} height={40}/>
+                          </div>
+                          ))}
+                        </div>
+                        <div className="h-5"/>
+
                             <p>{project.description}</p>
                             <br/>
-                          
                           {project.warning.length > 0 && 
                               <div className="text-yellow-500">⚠ {project.warning} </div>
                             }
